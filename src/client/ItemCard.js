1b7051ec69c5c89d31c378bec36dd451ad1ff6e0
Sheng <sandcforge@gmail.com>
2021-04-11 23:09:35 -0700

    fix bug

--- src/client/ItemCard.js
+++ src/client/ItemCard.js
@@ -70,7 +70,7 @@ export const ItemCard = (props) => {
           </Avatar>
         }
         title={details.GodAppTitle}
-        {/*subheader={`\u00a5${details.GodPresentPrice}`}*/}
+        subheader={`${details.GodSpecification}`}
       />
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>

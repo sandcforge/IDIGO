81803f86b9778c8d88e43d2bf3839af5e4987e9c
Sheng <sandcforge@gmail.com>
2021-05-22 10:48:02 -0700

    add router for the three pages:  homepage, not found  page and admin page.

---
+++ src/client/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {HomePage} from './HomePage';
+export const App = () => {
+  return (
+    <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/admin">
+          <div>管理员页面</div>
+          </Route>
+          <Route path="*">
+            <div>找不到页面！</div>
+          </Route>
+        </Switch>
+    </Router>
+  );
+}
--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HomePage} from './HomePage.js';
+import {App} from './App.js';
 
-ReactDOM.render(<HomePage />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
